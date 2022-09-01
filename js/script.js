1a4388e15dd9e7b992a209de49c9722a3f70758c
Jason Marinegit config --global user.name Jason <marinejason67@gmail.comgit config --global user.email marinejason67@gmail.com>
2022-08-31 19:32:20 -0700

    added background color

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const holder = document.querySelector('.message-holder')
 const message = document.querySelector('#message');
 const ball = document.querySelector('.ball');
-const messages = [ "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful","Imagine That","Anything is Poddible" ]
+const messages = [ "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful","anything is posibble" ]
 
 
 fadeInAnimation = function(){  
