6c50f1f4b8c16c4db969962ce4ee3c5815e3f886
Jason Marinegit config --global user.name Jason <marinejason67@gmail.comgit config --global user.email marinejason67@gmail.com>
2022-09-04 08:34:12 -0700

    added img

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@ const ball = document.querySelector('.ball');
 const messages = [ "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful","anything is posibble" ]
 
 
+const img = document.createElement("img");
+    img.src = "img/water-drops-6373296_1920.jpg";
+
+    const div = document.getElementById("x");
+    div.appendChild(img);
+    //block.setAttribute("style", "text-align:center");
+
+
+
 fadeInAnimation = function(){  
     setTimeout(function(){
     ball.classList.remove('shake');
