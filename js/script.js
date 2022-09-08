1aedd12e98133ee728d6191cc86fa8eda9e113e5
Jason Marinegit config --global user.name Jason <marinejason67@gmail.comgit config --global user.email marinejason67@gmail.com>
2022-09-07 19:59:26 -0700

    edit background

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 const holder = document.querySelector('.message-holder')
 const message = document.querySelector('#message');
 const ball = document.querySelector('.ball');
-const messages = [ "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful","anything is posibble" ]
-
-
-const img = document.createElement("img");
-    img.src = "img/water-drops-6373296_1920.jpg";
-
-    const div = document.getElementById("x");
-    div.appendChild(img);
-    //block.setAttribute("style", "text-align:center");
-
+const messages = [ "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful","anything is possible",];
 
 
 fadeInAnimation = function(){  
@@ -29,7 +20,7 @@ fadeOutAnimation = function(callback){
   callback();
 }
 
-ball.addEventListener('click', function() {
+ball.addEventListener('click', function() {;
   ball.classList.add('shake');
   fadeOutAnimation(fadeInAnimation);
 
